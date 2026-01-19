278a99c713f8d970a94ef9982dcbec298b01ee4c
dev <devanshc5@gmail.com>
2026-01-20 00:08:27 +0530

    a

---
+++ sb-ecom/src/main/java/com/ecommerce/project/model/OrderItem.java
@@ -0,0 +1,32 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "order_items")
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id",  referencedColumnName = "productId", nullable = true)
+    @org.hibernate.annotations.OnDelete(action = org.hibernate.annotations.OnDeleteAction.SET_NULL)
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private Integer quantity;
+    private double discount;
+    private double orderedProductPrice;
+
+}
